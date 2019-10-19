f7e3bf34d1a4fedc42541eb794f41e6cdf8d981f
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2019-10-20 00:04:14 +0200

    Created main.py, filled documentation
    Defined draft docstrings and added draft type annotations.
    The goal of this change is to make the code more readible and easy to access for someone that is reading it first time.

--- PACAnalysis.py
+++ PACAnalysis.py
@@ -12,9 +12,14 @@ from sc2reader.engine.plugins.pacanalyzer import *
 
 sc2reader.engine.register_plugin(PACAnalyzer())
 
+def printReplay(filepath:str, analysis:dict, arguments):
+    """
+    Prints summary information about SC2 replay file
+    filepath (str) - path to the file.
+    analysis (dict) - dictionary holding information on players.
+    arguments (parser.parse_args()) - object containing arguments for further processing.
+    """
 
-def printReplay(filepath, analysis, arguments):
-    """ Prints summary information about SC2 replay file """
     replay = sc2reader.load_replay(filepath, debug=True)
 
     if arguments.displayreplays:
@@ -62,59 +67,3 @@ def printReplay(filepath, analysis, arguments):
                 if arguments.displayreplays:
                     print("      \t{0} ({1})".format(player.name, player.pick_race[0]))
     print
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="""Prints PAC information from Starcraft II replay files or directories.""")
-    parser.add_argument('--recursive', action="store_false", default=True,
-                        help="Recursively read through directories of Starcraft II files [default on]")
-
-    required = parser.add_argument_group('Required Arguments')
-    required.add_argument('paths', metavar='filename', type=str, nargs='+',
-                          help="Paths to one or more Starcraft II files or directories")
-
-    display = parser.add_argument_group('Display Options')
-    display.add_argument('--pausestats', action="store_false", default=True,
-                        help="Pauses after each stat block [default on]")
-    display.add_argument('--pausereplays', action="store_true", default=False,
-                        help="Pauses after each replay summary [default off]")
-    display.add_argument('--displayreplays', action="store_true", default=False,
-                        help="Displays individual replay summaries [default off]")
-
-    arguments = parser.parse_args()
-    analysis = {}
-    depth = -1 if arguments.recursive else 0
-    replays = set(filepath for path in arguments.paths for filepath in utils.get_files(path, depth=depth) if os.path.splitext(filepath)[1].lower() == '.sc2replay')
-    replayCount = len(replays)
-
-    if replayCount == 1:
-        arguments.displayreplays = True
-        arguments.pausereplays = True
-
-    for replay in replays:
-        try:
-            printReplay(replay, analysis, arguments)
-        except:
-            print("Error with '{0}': ".format(replay))
-            replayCount -= 1
-        if arguments.pausereplays:
-            print("PRESS ENTER TO CONTINUE")
-            input()
-
-    if replayCount > 1:
-        print("\n--------------------------------------")
-        print("Results - {0} Replays Analyzed".format(replayCount))
-        print("{0} Players Analyzed".format(len(analysis)))
-        for stats in sorted(analysis.items(), key=lambda t: t[1].count, reverse=True):
-            print("\t{0:<15} (pid: {1})\t- {2} replays".format(stats[1].name, stats[0], stats[1].count))
-            print("\t\tPPM: {0:>6.2f}".format(stats[1].ppm))
-            print("\t\tPAL: {0:>6.2f}".format(stats[1].pal))
-            print("\t\tAPP: {0:>6.2f}".format(stats[1].app))
-            print("\t\tGAP: {0:>6.2f}".format(stats[1].gap))
-            if arguments.pausestats:
-                print("PRESS ENTER TO CONTINUE")
-                input()
-
-if __name__ == '__main__':
-    main()
